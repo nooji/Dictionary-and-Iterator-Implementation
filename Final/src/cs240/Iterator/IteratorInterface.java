074a34821b1734c33f0d4051f840e479f6f68417
cnguyen <thisiskevinnguyen@gmail.com>
2017-03-12 22:17:06 -0700

    Added comments to interface
    Signed-off-by: cnguyen <thisiskevinnguyen@gmail.com>

--- Final/src/cs240/Iterator/IteratorInterface.java
+++ Final/src/cs240/Iterator/IteratorInterface.java
@@ -1,6 +1,14 @@
 package cs240.Iterator;
 
 public interface IteratorInterface<T> {
+	/** Returns true if the iteration has more elements. 
+    @return true if the iteration has more elements */
 	public boolean hasNext();
+	/** Retrieves the next entry in the collection and
+	advances this iterator by one position.
+	@return a reference to the next entry in the iteration,
+	if one exists
+	@throws NoSuchElementException if the iterator had reached the
+	end already, that is, if hasNext() is false */
 	public T next();
 }
